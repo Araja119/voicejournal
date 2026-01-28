8f8afb463a39c8ab20b24f080c5dc0f7e5c7041d
Abdullah Raja <araja1685@gmail.com>
2026-01-28 13:57:54 -0500

    Initial Commit

---
+++ VoiceJournal/VoiceJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceJournalApp.swift
+//  VoiceJournal
+//
+//  Created by Araja  on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct VoiceJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
